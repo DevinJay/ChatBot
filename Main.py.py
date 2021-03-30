e7d4f6c4ecb51286561d565542e0ee6d4f62f302
Pasan Jayawardene <pj799571@gmail.com>
2021-03-30 12:20:58 +0530

    main.py

--- Main.py.py
+++ Main.py.py
@@ -17,6 +17,7 @@ import tflearn
 import tensorflow as tf
 import random
 import json
+import pickle
 
 
 # In[6]:
@@ -29,79 +30,80 @@ data
 
 # In[7]:
 
+try:
+	with open("data.pickle", "rb") as f:
+		words,labels,training,output=pickle.load(f)
 
-words=[]
-labels=[]
-docs_x=[]
-docs_y=[]
+except:
 
-
-# In[8]:
-
-
-nltk.download('punkt')
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds=nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-        
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+	words=[]
+	labels=[]
+	docs_x=[]
+	docs_y=[]
 
 
-# In[9]:
+	# In[8]:
 
 
-words=[stemmer.stem(w.lower()) for w in words if w != "?"]
-words= sorted(list(set(words)))
-
-labels=sorted(labels)
-
-
-# In[10]:
-
-
-training=[]
-output=[]
-
-
-# In[11]:
+	nltk.download('punkt')
+	for intent in data["intents"]:
+	    for pattern in intent["patterns"]:
+	        wrds=nltk.word_tokenize(pattern)
+	        words.extend(wrds)
+	        docs_x.append(wrds)
+	        docs_y.append(intent["tag"])
+	        
+	        if intent["tag"] not in labels:
+	            labels.append(intent["tag"])
 
 
-out_empty=[0 for _ in range(len(labels))]
-
-for x,doc in enumerate(docs_x):
-    bag=[]
-    wrds=[stemmer.stem(w) for w in doc]
-    
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-            
-    output_row=out_empty[:]
-    output_row[labels.index(docs_y[x])]=1
-    
-    training.append(bag)
-    output.append(output_row)
+	# In[9]:
 
 
-# In[12]:
+	words=[stemmer.stem(w.lower()) for w in words if w != "?"]
+	words= sorted(list(set(words)))
+
+	labels=sorted(labels)
 
 
-training=np.array(training)
-output=np.array(output)
+	# In[10]:
 
 
-# In[14]:
+	training=[]
+	output=[]
 
 
-training.shape
-output.shape
+	# In[11]:
 
+
+	out_empty=[0 for _ in range(len(labels))]
+
+	for x,doc in enumerate(docs_x):
+	    bag=[]
+	    wrds=[stemmer.stem(w) for w in doc]
+	    
+	    for w in words:
+	        if w in wrds:
+	            bag.append(1)
+	        else:
+	            bag.append(0)
+	            
+	    output_row=out_empty[:]
+	    output_row[labels.index(docs_y[x])]=1
+	    
+	    training.append(bag)
+	    output.append(output_row)
+
+
+
+
+
+	training=np.array(training)
+	output=np.array(output)
+
+	
+	with open("data.pickle", "wb") as f:
+		pickle.dump((words,labels,training,output), f)
 
 # In[3]:
 
@@ -119,15 +121,38 @@ model=tflearn.DNN(net)
 
 # In[15]:
 
+# try:
+model.load("model.tflearn")
+# 	1
+# except:
+#model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+#model.save("model.tflearn")
 
-model.fit(training,output, n_epoch=1000,batch_size=8,show_metric=True)
-model.save("model.tflearn")
+def bag_of_words(s,words):
+	bag=[0 for _ in range(len(words))]
+	
+	s_words=nltk.word_tokenize(s)
+	s_words=[stemmer.stem(word.lower()) for word in s_words ]
+
+	for se in s_words:
+		for i,w in enumerate(words):
+			if w== se:
+				bag[i]=1
+
+	return np.array(bag)
+
+def chat():
+	print("Start talking with the bot! (type quit to stop)")
+	while True:
+		inp= input("You: ")
+		if inp.lower() == "quit":
+			break
+
+		results=model.predict([bag_of_words(inp,words)])
+		print(results)
+
+chat()
 
 
-# In[ ]:
 
-
-import pickle
-with open("data.pickle", "wb") as f:
-    pickle.dump((words,labels,training,output),f)
 
